f88b50eb7be6f026effb9ca9880526ea29f57908
Tanu-ptdr06 <tanupatidar855@gmail.com>
2025-01-31 07:45:33 +0000

    study about time

---
+++ date_and_time_in_depth.js
@@ -0,0 +1,11 @@
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toTimeString());
+
+console.log(typeof myDate);
+
+let myCreateDate=new Date(2025,5,30)
+console.log(myCreateDate.toDateString());
+
