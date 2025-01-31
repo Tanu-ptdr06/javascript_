c297ef0936e4d17cb297f578c1d43ceddfb90467
Tanu-ptdr06 <tanupatidar855@gmail.com>
2025-01-31 03:56:47 +0000

    Your commit message here

---
+++ stack_and_heap.js
@@ -0,0 +1,8 @@
+let myYoutubename="tanuptdr"
+let anotherName=myYoutubename
+
+console.log(anotherName);
+
+//all primitive datatypes goes in stack and they got a copy of original data
+
+//in all non primitive datatype goes in  heap all have original reference of data
