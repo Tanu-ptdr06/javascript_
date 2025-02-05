01781b39f799a1116419dcef92562d5cb8291797
Tanu-ptdr06 <tanupatidar855@gmail.com>
2025-02-05 08:00:27 +0000

    about loop

---
+++ loop.js
@@ -0,0 +1,15 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+}
+let index=0
+while(index<=10){
+    console.log(`dhfjdkj ${index}`);
+        index=index+1
+}
+
+
+let score=11
+do{
+console.log(`hdsjkfhfj ${score}`);
+}while(score<=10);
