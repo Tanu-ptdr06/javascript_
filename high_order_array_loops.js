1a6818fe6b4440cb91786a43c83dbcbb1206eeaf
Tanu-ptdr06 <tanupatidar855@gmail.com>
2025-02-07 05:09:48 +0000

    study about loops

---
+++ high_order_array_loops.js
@@ -0,0 +1,42 @@
+//[{},{},{}]
+
+const greeting="Hello World"
+for(const greet of greeting){
+    console.log(`each char is ${greet}`);
+}
+
+
+//maps
+const map=new Map()
+map.set('IN',"india")
+map.set('USA',"america")
+console.log(map);
+
+//loop on map
+for(const [key,val] of map){
+    console.log(key);
+}
+
+
+//for in loop 
+const object={
+    js:'javascript',
+    cpp:'c++',
+    rb:"ruby"
+}
+
+for(const key in object){
+    //console.log(key);//for accessing key
+    console.log(object[key]);//for accessing value 
+
+}
+
+
+
+const coding=["jis","cpp","c","python"]
+coding.forEach( function (item){
+    console.log(item);
+})//this is call back so there is no name
+
+
+
